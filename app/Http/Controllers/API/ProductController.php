733ddad0a329f1fb0d8f2fbc03791222e1dc243b
AliUse <121191688+AliUse@users.noreply.github.com>
2026-03-29 15:48:55 +0200

    hinzufügen index() function für API

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+use App\Http\Resources\ProductResource;
+
 class ProductController extends Controller
 {
-    //
+    public function index() {
+        $products = Product::with('category')->paginate(10); 
+   // return response()->json($products);
+   return ProductResource::collection($products);
+    }
 }
