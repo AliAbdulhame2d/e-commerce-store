edea6a328dda1b0609c6a2b09815a40bc2e65bc1
AliUse <121191688+AliUse@users.noreply.github.com>
2026-03-29 14:32:36 +0200

    Debuggen

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
 class AuthController extends Controller
 {
 
