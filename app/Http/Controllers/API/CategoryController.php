a02d3f918a75aafdc8f94b726b915bf6753755d0
AliUse <121191688+AliUse@users.noreply.github.com>
2026-04-04 14:30:21 +0200

    erstellen Controllers für API
    CategoryController und ProductController

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+use App\Http\Requests\API\StoreCategoryRequest;
+
+class CategoryController extends Controller
+{
+  public function index() {
+    $categories = Category::paginate(10);
+    return CategoryResource::collection($categories);
+  } 
+
+  public function store(StoreCategoryRequest $request) {
+    $data = $request->validated();
+    $category = Category::create($data);
+    return new CategoryResource($category);
+  }
+
+  public function destroy(Category $category) {
+    $category->delete();
+    return response()->json(["message" => "Category deleted Successfully"]);
+  }
+
+
+}
--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Http\Requestes\API\StoreProductRequest;
 
 use App\Models\Product;
 use App\Http\Resources\ProductResource;
@@ -12,7 +12,25 @@ class ProductController extends Controller
 {
     public function index() {
         $products = Product::with('category')->paginate(10); 
-   // return response()->json($products);
-   return ProductResource::collection($products);
+        // return response()->json($products);
+        return ProductResource::collection($products);
+    }
+
+    public function store(StoreProductRequest $request) {
+        $data = $request->validated();
+        $product = Product::create($data);
+        return new ProductResource($product);
+    }
+
+    public function update(StoreProductRequest $request) {
+        $data = $request->validated();
+        $product = Product::findOrFail($id);
+        $product->update($data);
+        return new ProductResource($product);
+    }
+
+    public function destroy(Product $product) {
+        $product->delete();
+        return response()->json(["message" => "Product deleted successfully"]);
     }
 }
