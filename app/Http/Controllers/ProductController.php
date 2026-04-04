acb0f1d510e8ac3e3e58dc9dadc03d7123133c87
AliUse <121191688+AliUse@users.noreply.github.com>
2026-04-04 14:31:02 +0200

    Verbsserung

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,15 +9,45 @@ use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 
 class ProductController extends Controller
-{
-
-    
+{  
     /* ---------------- Products ---------------- */
 
     //Zeigen alle Produkte
     public function index() {
         $products = Product::with('category')->paginate(10); //get(); 
         return view('admin.products.index', compact('products'));
+    }
+
+    public function store(StoreProductRequest $request) {
+        $data = $request->validated();
+       
+        if($request->hasFile('image')) {
+            $data['image'] = $request->file('image')->store('products', 'public');
+        }
+    
+        Product::create($data);
+       
+    return redirect()->back()->with('success', 'Product Added Successfully');    
+    }
+
+    public function update(StoreProductRequest $request, $id) { 
+        
+        $data = $request->validated();
+        $product = Product::findOrFail($id);
+        
+        if ($request->hasFile('image')) {
+            //löchen das alte Bild
+            if ($product->image) {
+                \Storage::disk('public')->delete($product->image);
+            }
+            //speichern das Bild in Storage-Ordner
+            $data['image'] = $request->file('image')->store('products', 'public');
+        }
+
+        $product->update($data);
+
+        return redirect()->route('products.index')
+            ->with('success', 'Product Updated Successfully');
     }
 
     //Zeigen Add-Produkt-GUI, um ein neues Produkt zu hinzufügen
@@ -41,38 +71,6 @@ class ProductController extends Controller
 
         $product->delete();
         return redirect()->back()->with('success', 'Product Deleted Successfuly');
-    }
-
-    public function store(StoreProductRequest $request) {
-        $data = $request->validated();
-       
-        if($request->hasFile('image')) {
-            $data['image'] = $request->file('image')->store('products', 'public');
-        }
-    
-        Product::create($data);
-       
-    return redirect()->back()->with('success', 'Product Added Successfully');    
-    }
-
-    public function update(StoreProductRequest $request, $id) { 
-        
-        $product = Product::findOrFail($id);
-        $data = $request->validated();
-
-        if ($request->hasFile('image')) {
-            //löchen das alte Bild
-            if ($product->image) {
-                \Storage::disk('public')->delete($product->image);
-            }
-            //speichern das Bild in Storage-Ordner
-            $data['image'] = $request->file('image')->store('products', 'public');
-        }
-
-        $product->update($data);
-
-        return redirect()->route('products.index')
-            ->with('success', 'Product Updated Successfully');
     }
 
 }
